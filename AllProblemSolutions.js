f9da3f5510fb6ba828fed2fca14dbce5f928de42
Ian McCunn <ianmccunn@gmail.com>
2014-12-19 19:09:04 -0800

    s

--- AllProblemSolutions.js
+++ AllProblemSolutions.js
@@ -287,7 +287,7 @@ function exOh(str) {
 // and return the string true if the parameter is a palindrome, (the string is the same forward as it is backward) 
 // otherwise return the string false. For example: "racecar" is also "racecar" backwards. Punctuation and numbers will not be part of the string. 
 
-function isPalindrom(str) {
+function isPalindrome(str) {
 	 return (str.split(' ').join('') === str.split(' ').join('').split('').reverse().join('')) ? 'true' : 'false';
 }
 
